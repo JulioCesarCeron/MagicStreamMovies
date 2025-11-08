ba31258cca84e8a02a19a3d9e5b80550953fa56d
Julio Cesar Ceron <julio.ceorn@gmail.com>
2025-11-08 19:17:34 -0300

    Create movie controller

--- Server/MagicStreamMoviesServer/main.go
+++ Server/MagicStreamMoviesServer/main.go
@@ -3,18 +3,20 @@ package main
 import (
 	"fmt"
 
+	controllers "github.com/JulioCesarCeron/MagicStreamMovies/Server/MagicStreamMoviesServer/Server/MagicStreamMoviesServer/controller"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
 
-	router.GET("/hello", func (c *gin.Context) {
+	router.GET("/hello", func(c *gin.Context) {
 		c.String(200, "Hello, MagicStreamMovies!")
 	})
 
-	err := router.Run(":8080")
+	router.GET("/movies", controllers.GetMovies())
 
+	err := router.Run(":8080")
 	if err != nil {
 		fmt.Println("Failed to start server", err)
 	}
