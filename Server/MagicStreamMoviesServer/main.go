97dbe8545b090221de32081eca5cdd2c22723d84
Julio Cesar Ceron <julio.ceorn@gmail.com>
2025-11-08 17:47:48 -0300

    Create start server function

---
+++ Server/MagicStreamMoviesServer/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/hello", func (c *gin.Context) {
+		c.String(200, "Hello, MagicStreamMovies!")
+	})
+
+	err := router.Run(":8080")
+
+	if err != nil {
+		fmt.Println("Failed to start server", err)
+	}
+}
