00f5510e2fc2de08b1ef2597bf4c8810b4285240
Julio Cesar Ceron <julio.ceorn@gmail.com>
2025-11-11 21:58:17 -0300

    Create database connection

---
+++ Server/MagicStreamMoviesServer/database/database_connection.go
@@ -0,0 +1,54 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+func Connect() *mongo.Client {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Println("Warning: unable to find .env file")
+	}
+
+	MongoDb := os.Getenv("MONGODB_URI")
+
+	if MongoDb == "" {
+		log.Fatal("MONGODB_URI not found in environment variables")
+	}
+
+	fmt.Println("MongoDB URI: ", MongoDb)
+
+	clientOptions := options.Client().ApplyURI(MongoDb)
+
+	client, err := mongo.Connect(clientOptions)
+	if err != nil {
+		log.Fatal("Error connecting to MongoDB:", err)
+	}
+
+	return client
+}
+
+var Client *mongo.Client = Connect()
+
+func OpenCollection(collectionName string) *mongo.Collection {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Println("Warning: .env files could not be found.")
+	}
+
+	databaseName := os.Getenv("DATABASE_NAME")
+
+	collection := Client.Database(databaseName).Collection(collectionName)
+
+	if collection == nil {
+		return nil
+	}
+
+	return collection
+}
